b83ae126b164a9c97fc2446d1a9ef0149e3a5ea1
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2025-02-07 16:59:08 -0500

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -92,7 +92,7 @@ chat_html = """
         <title>Chat Widget</title>
     </head>
     <body>
-        <div style="width: 100%; height: 400px;">
+        <div style="width: 100%; height: 100px;">
             <elevenlabs-convai agent-id="gMh8bGtmxS5OxxPwDuKT"></elevenlabs-convai>
         </div>
         <script src="https://elevenlabs.io/convai-widget/index.js" async></script>
@@ -130,8 +130,8 @@ with st.sidebar:
         """)
 
 # Imagen principal
-image = Image.open('data_analisis.png')
-st.image(image, use_column_width=True)
+#image = Image.open('data_analisis.png')
+#st.image(image, use_column_width=True)
 
 # Carga de archivo
 st.subheader("📁 Carga de Datos")
