ae061f22c969341cc2c902107dd437390786e4cc
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2025-02-07 17:16:14 -0500

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -99,7 +99,7 @@ chat_html = """
     </body>
     </html>
 """
-st.components.v1.html(chat_html, height=250, scrolling=True)
+st.components.v1.html(chat_html, height=150, scrolling=False)
 st.markdown("---")
 
 # Sidebar
